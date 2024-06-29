5402298b5c533a8fe1b978f244579043aa6e7a1d
iamdipeeka <152793823+iamdipeeka@users.noreply.github.com>
2024-06-29 18:55:33 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,18 @@
+//same datatype ka comparison to easy hota h pr alg alg..
+console.log(2>1);
+console.log(5>1);
+
+
+//for different datatype comparison
+console.log(("2">1));
+console.log("07">1);
+
+// >,=> and == alg alg treat krte h
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+//===
+
+console.log("2"===2)
