0b3f229a91312623af92483cf4c43e451c3502a4
iamdipeeka <152793823+iamdipeeka@users.noreply.github.com>
2024-06-29 10:20:11 +0000

    variable and datatypes done

---
+++ 01_basics/01_var.js
@@ -0,0 +1,12 @@
+const accountId = 1334564
+let Email_Id = "anisha@gmail.com"
+var accountPassword ="134567"
+ accountCity = "vellore"
+ let accountState;
+
+ Email_Id = "deep@gmail.com"
+ accountPassword = "333333"
+ accountCity = "benglore"
+
+
+ console.table([accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"
+
+//alert("hello")
+//alert(3+3) //ese browser m use krte h
+ //readibility shi honi chaiye
+
+ console.log("hitesh");
+ let age = 12;
+ 
+
+ console.log(typeof "hitesh")
+ console.log(typeof null)
+ console.log(typeof undefined)
