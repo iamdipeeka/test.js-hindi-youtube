027ea5927faa62769fa7cdc9d98473156fe2e1ac
iamdipeeka <152793823+iamdipeeka@users.noreply.github.com>
2024-06-30 18:42:09 +0000

    aaray1

---
+++ 02_basics/Arrays1.js
@@ -0,0 +1,43 @@
+//Arrays
+// const Myarr = [1,2,3,'hitesh',6,6]
+
+const Myarr = [0,1,2,3,4]
+// const myHeroes = ['shaktiman','powerranger']
+
+// const Myarr2 = new Array(1,2,3,4)
+// console.log(Myarr[2])
+
+
+//*****methods of arrays*****
+
+// Myarr.push(6)
+// Myarr.push(7)
+// Myarr.pop()
+
+// Myarr.unshift(9)
+// console.log(Myarr);
+// Myarr.shift()
+
+// console.log(Myarr);
+
+console.log(Myarr.includes(8));
+console.log(Myarr.indexOf(7));
+
+console.log(Myarr);
+
+
+//slice,splice
+
+console.log('A', Myarr);
+const myn1 = Myarr.slice(1,3)
+console.log(myn1);
+
+console.log('B ', Myarr);
+
+const myn2 = Myarr.splice(1,3)
+console.log(myn2);
+
+console.log('C',Myarr);
+
+
+
