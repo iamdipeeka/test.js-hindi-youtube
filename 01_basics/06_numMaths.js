932cd096a4d1626fbe03ef5cfb2afd7760e87128
iamdipeeka <152793823+iamdipeeka@users.noreply.github.com>
2024-06-30 11:56:17 +0000

    math

--- 01_basics/06_numMaths.js
+++ 01_basics/06_numMaths.js
@@ -1,5 +1,34 @@
 const score = 400;
 console.log(score);
 
-const balance = new Number (200);
+const balance = new Number (200.99);
 console.log(balance);
+
+console.log(balance.toFixed(3));
+console.log(balance.toPrecision(1));
+console.log(balance.toString());
+
+const anotherbalance = 100000
+console.log(anotherbalance.toLocaleString('en-IN'));
+
+
+//*************maths************ 
+
+
+console.log(Math)
+console.log(Math.abs(-76));
+console.log(Math.round(4.8));
+console.log(Math.floor(4.8));
+console.log(Math.ceil(3.1));
+console.log(Math.min(4,6,9,10));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10 )+1); 
+//for getting minimun 1 value
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
