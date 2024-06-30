3774a4f51d77e0fb74ba36fb771510cc23f05ac5
iamdipeeka <152793823+iamdipeeka@users.noreply.github.com>
2024-06-30 07:39:17 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name = "hitesh"
+const repoCount = 50
+
+
+console.log(`My name is ${name} and my repocount is ${repoCount}`);
+
+const Gamename = new String ('hiteshcc')
+
+console.log(Gamename[0]);
+console.log(Gamename.length);
+
+console.log(Gamename.charAt(5));
+console.log(Gamename.indexOf('s'));
+
+const newstring = Gamename.substring(0,5)
+console.log(newstring);
+
+const anothernewstring = Gamename.replace('c','i')
+console.log(anothernewstring);
+
+const newstringone = Gamename.slice(-6,8)
+console.log(newstringone);
+
+const newstringtwo = '    mahi   '
+console.log(newstringtwo.trim());
