4902c87a886a81c5984bcfb482557e3bf18fe5e2
iamdipeeka <152793823+iamdipeeka@users.noreply.github.com>
2024-07-01 16:20:52 +0000

    arrays

--- 02_basics/Objects2.js
+++ 02_basics/Objects2.js
@@ -24,3 +24,27 @@ const obj2 ={3:'a',2:'b'}
 //const obj3 = Object.assign({},obj1,obj2)
 const obj3 = {...obj1,...obj2}
 console.log(obj3);
+
+const users = {
+    id :1,
+    email:"hag@gmail.com"
+}
+{
+     id :1,
+    emaill;"hag@gmail.com"
+}
+{
+     id :1,
+    emailll;"hag@gmail.com"
+}
+{
+     id :1,
+    email;"hag@gmail.com"
+}
+
+users[1].email
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
